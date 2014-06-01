2b8e1e98116f4105e48502a18d6f691f0de2d52b
Savin Yurii <github@savinyurii.ru>
2014-06-01 12:56:22 +0900

    комментарии к get_groups

--- site/get/get_groups.php
+++ site/get/get_groups.php
@@ -1,15 +1,15 @@
 <?php 
-header('Content-type: application/json; charset=utf-8');
-require "../include/mysql_connect.php";
-require "../include/json_encode.php";
+header('Content-type: application/json; charset=utf-8'); // принудительно использовать кодировку UTF-8
+require "../include/mysql_connect.php"; // Содержит инструкции подключения к БД
+require "../include/json_encode.php"; // Содержит исправленную реализацию функции json_encode()
 
-$faculty_id=$_GET['faculty_id'];
-if($faculty_id){
-	$query="SELECT group_id, group_name FROM groups WHERE faculty_id=$faculty_id";
+$faculty_id=$_GET['faculty_id']; // ID факультета берётся из параметра GET-запроса
+if($faculty_id){ // Проверка, что ID передан
+	$query="SELECT group_id, group_name FROM groups WHERE faculty_id=$faculty_id"; //SQL запрос
 	$result = mysql_query($query) or die('{"error": "'.mysql_error().'"}'); 
 	if($result){
 		while($row=mysql_fetch_assoc($result)){
-			$output[]=$row;
+			$output[]=$row; // формирование массива
 		}
 		echo '{"groups":'.json_encode_utf8($output).'}';
 	}
